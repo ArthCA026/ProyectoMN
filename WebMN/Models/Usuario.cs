aef5cdead81953e61b6b9dcffc0ed13c6e8be0a3
ArthCA026 <r2chaves026@gmail.com>
2023-10-25 12:18:13 -0600

    Commit 0 del proyecto

---
+++ WebMN/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebMN.Models
+{
+    public class Usuario
+    {
+
+        public string ConsultarNombre()
+        {
+            return "eduardo";
+
+        }
+    }
+}
